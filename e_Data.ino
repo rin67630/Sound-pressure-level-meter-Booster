2ddb850035f8adda056d9cb242010775a4046778
RIN67630 <lazlo.lebrun@googlemail.com>
2020-08-15 21:36:01 +0200

    Update e_Data.ino

--- e_Data.ino
+++ e_Data.ino
@@ -1,6 +1,5 @@
 void data125mSRun()
 {
-  //  ConOut.print("'");
 #if defined (SOUND_SOURCE_ANAIN)
   // === ( Short Leq from A0 ) ===
   // Performing 3 reads to get a reliable reading.
@@ -37,14 +36,6 @@ void data125mSRun()
   {
     sound.A0dBImpulse = sound.A0dBImpulse - 0.5375 ; //Linear decay 4,3db /sec
   }
-  sound.A0dBImpulse = max(sound.A0dBImpulse, sound.A0dBSlow);
-
-  // Background
-  if ( sound.A0dBFast < EVENT_THRESHOLD_LEVEL)  //Low pass filter ignoring events
-  {
-    sound.A0dBBgr += (sound.A0dBSlow - sound.A0dBBgr) / 16000;  // Low Pass filter 2000s
-  }
-
 #else
   yield();
 #endif
@@ -53,9 +44,7 @@ void data125mSRun()
 void data1SRun()
 {
   yield();
-  //  ConOut.print("Da");
-
-  // Fetching Noise level from URL
+  // Fetching Noise level from URL with reduced resolution
 #if defined (SOUND_SOURCE_URL)   // must be a JSON from DFLD.de
   HTTPClient http;              //Declare an object of class HTTPClient
   http.begin(DFLDjsonURL);      //Specify request destination
@@ -63,16 +52,20 @@ void data1SRun()
   if (httpCode > 0)             //Check the returning code
   {
     JSONpayload = http.getString();          //Get the request response payload
-    // it should be: {"generator":"DFLD_LIVE","version":"0.9","time":1587592375,"access-control":"ignoredWrongPWD","027":32.0}
-    JSONpayload = JSONpayload.substring(100, 104);
-
+    // it should be like: {"generator":"DFLD_LIVE","version":"0.9","time":1587592375,"access-control":"ignoredWrongPWD","027":32.0}
+    JSONpayload = JSONpayload.substring(100, 104); // taking only the dB value
     sound.A0dBFast = JSONpayload.toFloat();
-    A0Inst = 100 * sound.A0dBFast;
     sound.A0dBMin = min(sound.A0dBFast, sound.A0dBMin);
     sound.A0dBMax = max(sound.A0dBFast, sound.A0dBMax);
-    A0dBMin1min = min(sound.A0dBFast, A0dBMin1min);
-    A0dBMax1min = max(sound.A0dBFast, A0dBMax1min);
     sound.A0dBSlow += (sound.A0dBFast - sound.A0dBSlow) / 6; // Low Pass filter 6 sec
+    if ( sound.A0dBFast > sound.A0dBImpulse)           //A0 in dB with impulse response
+    {
+      sound.A0dBImpulse = sound.A0dBFast;              //No filter
+    }
+    else
+    {
+      sound.A0dBImpulse = sound.A0dBImpulse - 4.3 ; //Linear decay 4,3db /sec
+    }
   }
   http.end();   //Close connection
 #endif
@@ -81,18 +74,19 @@ void data1SRun()
 
   // I use a quick and dirty method called 'type punning' copying the memory block of a structure into an array of chars,
   // transmitting this array, and copying back the memory block of the array into the same structure on the other side.
-  // I dont use any header info, only the difference of size permits to assing the received packets to sound or battery.
+  // I dont use any header info, only the difference of size permits to assign the received packets to sound or battery.
   // it is quick and damn efficient, but NOT portable and YOU must take care to have the same structures on both systems
   // and different sizes for Battery and Sound...
 
   int packetSize = UDP.parsePacket();
-//  if (packetSize) ConOut.println(packetSize);
-  if (packetSize == sizeof(sound))
+  // if (packetSize) Serial.printf("Packet size: %u, Sound size: %u , Battery size; %u \n\r", packetSize, sizeof(sound), sizeof(battery));
+  if (packetSize) digitalWrite(STDLED, false);    // Blink built-in LED on received packets
+    if (packetSize == sizeof(sound))
   {
     UDP.read(soundPayload, UDP_TX_PACKET_MAX_SIZE);
     memcpy(&sound, soundPayload, sizeof(sound));
-    // ConOut.printf("Min:%2.1f Max:%2.1f Imp:%2.1f  Bgr:%2.1f Slow:%2.1f\n", sound.A0dBMin, sound.A0dBMax, sound.A0dBImpulse, sound.A0dBBgr, sound.A0dBSlow);
-//     ConOut.print("s");
+    // ConOut.printf("Min:%2.1f Max:%2.1f Imp:%2.1f  Bgr:%2.1f Slow:%2.1f\n\r", sound.A0dBMin, sound.A0dBMax, sound.A0dBImpulse, sound.A0dBBgr, sound.A0dBSlow);
+    //     ConOut.print("s");
   }
   if (packetSize == sizeof(battery))
   {
@@ -102,33 +96,33 @@ void data1SRun()
     memcpy(&battery, batteryPayload, sizeof(battery));
     delta_voltage = battery.voltage - m;
     delta_current = battery.current - n;
-    // ConOut.printf("Ah: %2.1f, Volt: %2.1f, Amp: %2.1f, Watt: %2.1f, %%Batt: %2.1f\n", AhBat, battery.voltage , battery.current , battery.power , percent_charged);
-//     ConOut.print("b");
+    // ConOut.printf("Ah: %2.1f, Volt: %2.1f, Amp: %2.1f, Watt: %2.1f, %%Batt: %2.1f\n\r", AhBat, battery.voltage , battery.current , battery.power , percent_charged);
+    delay(3);    // let built-in LED blink slightly stronger on battery packet 
   }
-
+  digitalWrite(STDLED, true);
 #endif
 
   //DFLD Formatting (AK Modulbus format)
   sound.A0dBAK = 50 + int(2 * sound.A0dBFast);                  // Reduction to 1 Byte 50...250
   A0dBArray[60 * Minute + Second] = sound.A0dBAK;
+  sound.A0dBImpulse = max(sound.A0dBImpulse, sound.A0dBSlow);
 
-  // Noise averaging
+  // Computing Background noise level with a low pass filter ignoring events
+  if ( sound.A0dBFast < EVENT_THRESHOLD_LEVEL)
+  {
+    sound.A0dBBgr += (sound.A0dBSlow - sound.A0dBBgr) / 16000;  // Low Pass filter 2000s
+  }
+
+  // Noise averaging & 60s Max-Min
   A0dBSum6S += sound.A0dBSlow;  // Noise simple Averaging 6 Seconds
   nA0dB6S++;
   A0dB6S =  A0dBSum6S / nA0dB6S;
+  A0dBMin1min = min(sound.A0dBSlow, A0dBMin1min);
+  A0dBMax1min = max(sound.A0dBImpulse, A0dBMax1min);
 
-  if ( sound.A0dBFast > sound.A0dBImpulse)           //A0 in dB with impulse response
-  {
-    sound.A0dBImpulse = sound.A0dBImpulse + (A0dB6S - sound.A0dBImpulse) / 4;
-  }
-  else
-  {
-    sound.A0dBImpulse = sound.A0dBImpulse + (A0dB6S - sound.A0dBImpulse) / 150;
-  }
-
-  //===============================================
-
-  // Process NAT statistics
+//===============================================
+// Process NAT statistics
+  
   if (sound.A0dBSlow > EVENT_THRESHOLD_LEVEL && state != 'e') //
   {
     if (sound.A0dBSlow > peakValue)
@@ -240,21 +234,23 @@ void data1SRun()
       state = 'e';
   } // end Switch
 
-  //===============================================
-  // Noise Lequ integration
+//===============================================
+// Process Noise Lequ integration
 
   A0dBSumExp1min  =  A0dBSumExp1min  + pow(10, sound.A0dBSlow / 10);    // 1.Computation of Lequ Sum of Exp...
   if (MinuteExpiring)
   {
-    // Noise Lequ Hourly result
-    A0dBLeq1min = 10 * log10(A0dBSumExp1min / 60);   // 2.Computation of Lequ Log of Sum of Exp
+    // Noise Lequ Minute result
+    A0dBLeq1min = 10 * log10(A0dBSumExp1min / 60);                      // 2.Computation of Lequ Log of Sum of Exp
+  if  (A0dBLeq1min < 26)  A0dBLeq1min = LOWER_LIMIT_DB;                 // Initialize at 26dB minimum
+  A0dBSumExp60min  =  A0dBSumExp60min  + pow(10, A0dBLeq1min / 10);     // 1.Computation of Lequ Sum of Exp...
   }
 
-  A0dBSumExp60min =  A0dBSumExp60min + pow(10, sound.A0dBSlow / 10);    // 1.Computation of Lequ Sum of Exp...
   if (HourExpiring)
   {
     // Noise Lequ Hourly result
-    leq[Hour] = 10 * log10(A0dBSumExp60min / 3600);   // 2.Computation of Lequ Log of Sum of Exp
+    leq[Hour] = 10 * log10(A0dBSumExp60min / 60);   // 2.Computation of Lequ Log of Sum of Exp
+    A0dBSumExp60min  =  0;
     leq[25] = leq[Hour];
 
     // Noise Lequ Daily results
@@ -278,27 +274,27 @@ void data1SRun()
 
       leq[30] = (leq[22] + leq[23]) / 2;            // 30 =L22-24h
 
-
+      // NAT 0...23= hourly NAT , 25=current event 26=Nat24h 27= Daytime 28= Nighttime 29= 22h-24h
       NAT[27] = 0;                              // 27=NATuDaylight 6:00 - 22:00
       for  (byte n = 6; n < 22; n++)
       {
         NAT[27] = NAT[27] + NAT[n];
       }
       NAT[28] = NAT[22] + NAT[23] ;              // 28=NATuNight 22:00 - 06:00
-      for  (byte n = 0; n < 6; n++)
+      for  (byte n = 0; n < 6; n++)              // adding the 6 hours 0:00..6:00
       {
         NAT[28] = NAT[28] + NAT[n];
       }
-      NAT[26] = NAT[27] + NAT[28];        // 26=NAT24h
-
+      NAT[26] = NAT[27] + NAT[28];              // 26=NAT24h = NAT Night + NAT DAY
       NAT[29] = (NAT[22] + NAT[23]);            // 30 =L22-24h
     }
   }
   // end of just before new hour
 
+//===============================================
+// Measure Battery
 
 #if defined BATTERY_SOURCE_INA
-  //===============================================
   if (NewMinute)
   {
     float m = battery.voltage;
@@ -320,8 +316,9 @@ void data1SRun()
   // continuing either with values from above, or from UDP transmission
   // Evaluate battery health
   percent_charged = map(battery.voltage * 100, MIN_VOLT * 100, MAX_VOLT * 100, 0, 100);
-  // Calculate battery internal resistance if inside regular limits
-  if (fabs(delta_current) > 0.002 && battery.voltage < (MAX_VOLT - 0.5) && battery.voltage > (MIN_VOLT + 0.5)) internal_resistance = fabs(delta_voltage / delta_current);
+  
+  // Calculate battery internal resistance (r = dv / di) if inside regular limits and smooth it.
+  if (fabs(delta_current) > 0.005 && battery.voltage < (MAX_VOLT - 0.5) && battery.voltage > (MIN_VOLT + 0.5)) internal_resistance = internal_resistance + ((fabs(delta_voltage / delta_current)) - internal_resistance) / 10;
 
   // Daily Battery voltage comparison
   if (SecondOfDay == 14400) voltageAt4h  = battery.voltage;   // taking the voltage at 04:00 to evaluate if the battery gained/lost during the previous day.
@@ -349,8 +346,9 @@ void data1SRun()
     AhBat[26] = AhBat[27];
   }
 
+//===============================================
+// Getting Weather from OpenWeatherMap every 5 minutes
 #if defined WEATHER_SOURCE_URL
-  //===============================================  Getting Weather from OpenWeatherMap every 5 minutes
   if (Minute % 5 == 1 && Second == 32)   // call every 5 minutes
   { //check here: https://github.com/TridentTD/TridentTD_OpenWeather/blob/master/examples/WeatherNow/WeatherNow.ino
     myPlace.weatherNow();
@@ -364,5 +362,4 @@ void data1SRun()
     sunset               = myPlace.readSunset(TZ);
   }
 #endif
-}
-// end of 1S run
+} // end of 1S run
